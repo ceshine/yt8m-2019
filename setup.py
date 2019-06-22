de7cd5ca337c66b763fa2e54c90197ceff4f411d
CeShine Lee <shuanck@gmail.com>
2019-06-22 18:28:49 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyTorchHelperBot',
-    version='0.1.4',
+    version='0.1.5',
     packages=['helperbot', ],
     install_requires=[
         'torch>=0.4.1',
