7d135230a8f3023e3b33aa0d800e3a4420c812e8
CeShine Lee <shuanck@gmail.com>
2019-07-13 17:48:38 +0800

    Fix LRScheduler PyTorch 1.1 Compatibility Issue

--- helperbot/lr_scheduler.py
+++ helperbot/lr_scheduler.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from torch.optim.lr_scheduler import _LRScheduler
 from torch.optim import Optimizer
 
@@ -20,6 +22,7 @@ class BaseLRScheduler(_LRScheduler):
         if last_epoch == -1:
             for group in optimizer.param_groups:
                 group.setdefault('initial_lr', group['lr'])
+            last_epoch = 0
         else:
             for i, group in enumerate(optimizer.param_groups):
                 if 'initial_lr' not in group:
@@ -27,8 +30,27 @@ class BaseLRScheduler(_LRScheduler):
                                    "in param_groups[{}] when resuming an optimizer".format(i))
         self.base_lrs = list(
             map(lambda group: group['initial_lr'], optimizer.param_groups))
-        self.step(last_epoch + 1)
         self.last_epoch = last_epoch
+
+        # New in PyTorch 1.1.0
+
+        # Following https://github.com/pytorch/pytorch/issues/20124
+        # We would like to ensure that `lr_scheduler.step()` is called after
+        # `optimizer.step()`
+
+        def with_counter(func, opt):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                opt._step_count += 1
+                return func(*args, **kwargs)
+            wrapper._with_counter = True
+            return wrapper
+        self.optimizer.step = with_counter(self.optimizer.step, self.optimizer)
+        self.optimizer._step_count = 0
+        self._step_count = 0
+
+        # Start from last_epoch
+        self.step(last_epoch)
 
 
 class TriangularLR(BaseLRScheduler):
