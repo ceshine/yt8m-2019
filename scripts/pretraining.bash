b6d09e595285f4f1c38e370723be8c7785d5086a
Ceshine Lee <shuanck@gmail.com>
2019-10-16 00:01:27 +0800

    Fix the pretraining script

--- scripts/pretraining.bash
+++ scripts/pretraining.bash
@@ -4,5 +4,5 @@ SEED=17805 python -m yt8m.train_video dbof --steps 100000 --ckpt-interval 10000 
 mv data/cache/video/baseline_model.pth data/cache/video/dbof-3.pth
 SEED=4827 python -m yt8m.train_video dbof --steps 120000 --ckpt-interval 10000 --lr 3e-4 --batch-size 32 --n-mixtures 5
 mv data/cache/video/baseline_model.pth data/cache/video/dbof-1.pth
-SEED=1635 python -m yt8m.train_video --steps 100000 --ckpt-interval 10000 --lr 4e-4  --batch-size 32 --max-len 200
+SEED=1635 python -m yt8m.train_video dbof --steps 100000 --ckpt-interval 10000 --lr 4e-4  --batch-size 32 --max-len 200
 mv data/cache/video/baseline_model.pth data/cache/video/dbof-2.pth
